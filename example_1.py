3f41437bacb49336823c52378dfa2537b4661f9e
hglee <hglee@cubox.aero>
2022-01-03 15:02:20 +0900

    ex1 - from printing str to summing nums

--- example_1.py
+++ example_1.py
@@ -5,32 +5,37 @@ from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import numpy as np
 
-str_list_to_print = ['a'] * 10 + ['b'] * 15 + ['c'] * 25000000
+target_list = [1] * 10 + [2] * 15 + [3] * 25000000
     
-def worker(p_id, data):
-    print(p_id, ' - ', data)
-    return data
+def worker(p_id, num_list):
+    result = 0
+    if p_id == 0:
+        num_list = tqdm(num_list)
+    for num in num_list:
+        result += num
+    
+    return result
 
 if __name__ == '__main__':
-    print('Single Process')
-    result_1 = []
-    for str in tqdm(str_list_to_print):
-        result_1.append(str)
-    print(len(result_1))
+    print('--- Single Process ---')
+    result_1 = 0
+    for num in tqdm(target_list):
+        result_1 += num
+    print(result_1)
     
-    print('Multi Process')  
+    print('--- Multi Process ---')  
     num_process = 3
     pool = ProcessPoolExecutor(num_process)
     futures = []
     
-    size_list = len(str_list_to_print)
+    size_list = len(target_list)
     partition = size_list // num_process
-    split_list = np.split(str_list_to_print, [partition * (idx + 1) for idx in range(num_process-1)])
+    split_list = np.split(target_list, [partition * (idx + 1) for idx in range(num_process-1)])
 
-    result_2_size = 0
+    result_2 = 0
     for p_id in range(num_process):
         futures.append(pool.submit(worker, p_id, split_list[p_id]))
     for future in as_completed(futures):
-        result_2_size += len(future.result())
+        result_2 += future.result()
     
-    print(result_2_size)
+    print(result_2)
